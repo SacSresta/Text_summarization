a23056089c7720e761c52b651e430928000ec492
SacSresta <141592782+SacSresta@users.noreply.github.com>
2024-10-17 12:36:40 +1100

    Update app.py "src"

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 from fastapi.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
 from fastapi.responses import Response
-from textSummarizer.pipeline.prediction import PredictPipeline
+from src.textSummarizer.pipeline.prediction import PredictPipeline
 
 
 text:str = "What is Text Summarization?"
