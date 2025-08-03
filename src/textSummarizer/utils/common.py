65fd1df4953b1c183d7ae8372236bff2c05c06a4
Sac Sresta <sachin.shrestha42@gmail.com>
2025-08-03 09:55:54 +0000

    Changes to Readme.md

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -2,7 +2,7 @@ import os
 from box.exceptions import BoxValueError
 import yaml
 from textSummarizer.logging import logger
-from ensure import ensure_annotations
+from typeguard import typechecked as ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
